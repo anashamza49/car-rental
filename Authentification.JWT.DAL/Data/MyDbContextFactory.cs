a6c0bafbd5972ac3f69f5267d1ebc76015c97da4
anass <anass_hamzaoui@hotmail.com>
2025-04-14 23:19:06 +0100

    managing db

---
+++ Authentification.JWT.DAL/Data/MyDbContextFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace Authentification.JWT.DAL.Data
+{
+    public class MyDbContextFactory : IDesignTimeDbContextFactory<MyDbContext>
+    {
+        public MyDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<MyDbContext>();
+            optionsBuilder.UseSqlServer("DefaultConnection", b => b.MigrationsAssembly("Authentification.JWT.DAL"));
+            return new MyDbContext(optionsBuilder.Options);
+        }
+    }
+}
